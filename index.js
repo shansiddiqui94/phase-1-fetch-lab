45023d894cd8a4135b6aecad467873e57fccb1cb
Shan <shansiddiqui94@gmail.com>
2024-02-07 14:16:48 -0500

    failing one test

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+let got = "https://anapioficeandfire.com/api/books";
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(got)
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks().then((books) => {
+    renderBooks(books);
+  });
 });
